652744403eb5d0e4982c8a74e715e0b39b01653b
Raiko Eckstein <mail@raikoeckstein.de>
2025-12-21 11:42:08 +0100

    more work done in chapter2

---
+++ chapter_02/full_name.py
@@ -0,0 +1,18 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+favorite_language.rstrip()
+print(favorite_language)
+
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
+print(nostarch_url)
---
+++ chapter_02/name.py
@@ -0,0 +1,4 @@
+name = "Ada Lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
