af5d97b953f40d655223a200c4beb12f3b06e88f
Raiko Eckstein <mail@raikoeckstein.de>
2025-12-22 13:50:33 +0100

    more chapter 4 work

---
+++ chapter_04/exercise_chapter_04.py
@@ -0,0 +1,19 @@
+# 4-3
+for number in range(1,21):
+    print(number)
+
+# 4-4
+million = list(range(1,1000001))
+print(million)
+print(f"Min: {min(million)}, Max: {max(million)}, Sum: {sum(million)}")
+
+odd_numbers = list(range(1,21,2))
+print(odd_numbers)
+three_multiples = list(range(3,31,3))
+for number in three_multiples:
+    print(number)
+
+cubes = [values**3 for values in range(1,11)]
+for cube in cubes:
+    print(cube)
+
---
+++ chapter_04/square_numbers.py
@@ -0,0 +1,8 @@
+squares = []
+for value in range(1, 11):
+    squares.append(value ** 2)
+
+print(squares)
+
+digits = list(range(10))
+print(f"Min {min(digits)}, Max {max(digits)}, Sum {sum(digits)}")
