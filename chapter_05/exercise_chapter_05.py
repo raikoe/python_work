2a4da49e459a245d040a2d8edb08cf1b4966a91d
Raiko Eckstein <mail@raikoeckstein.de>
2026-01-02 16:13:24 +0100

    chapter 5 work

---
+++ chapter_05/exercise_chapter_05.py
@@ -0,0 +1,8 @@
+# Exercise 5.4: Alien Colors #1
+alien_color = 'green'
+# ['green', 'yellow', 'red']
+
+if alien_color == 'green':
+    print("You just earned 5 points!")
+
+# Exercise 5.4: Alien Colors #2
